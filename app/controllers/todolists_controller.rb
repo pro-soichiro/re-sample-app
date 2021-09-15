955e524710657b2d725469c44ecaab0a04d6d1a1
EC2 Default User <ec2-user@ip-172-31-85-160.ec2.internal>
2021-09-15 00:35:51 +0000

    [Update] 2-5

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,19 @@
+class TodolistsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to todolists_path
+  end
+  def index
+    @lists = List.all
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'todolists/new'
+  get 'top' => 'homes#top'
+  post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
 end
