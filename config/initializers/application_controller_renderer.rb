1247fcb3f586c9c0e57f03b360b4ad52470ee951
EC2 Default User <ec2-user@ip-172-31-85-160.ec2.internal>
2021-09-16 01:52:46 +0000

    [Finish] Railsを学ぼう2周目

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2715641f9ad14ba9aafffb3d2204ace5fa550936ddb27ae51c0aa636418f2a10be79d430f8c302bd70e7a7f4dc5e3c4fb3e5cfd8928a6bc128e498d462be3f19'
